6294b665c7c81930c2dc0a713745a194441a0eed
Joe Egboka <joeegboka@gmail.com>
2023-05-05 09:51:09 +0100

    added header image

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,30 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const Header = () => {
-    return (
-        <div className='flex flex-wrap items-center justify-between px-20 xl:h-[70%] xl:w-[80%] mt-5'>
-            <div className=''>
-                <h1 className='text-xl'>{`HI, I'M EMMANUEL.`}<br/> <span className='font-bold'>A CREATIVE GRAPHICS DESIGNER</span></h1>
-            </div>
-            <div className='border rounded-lg mr-20 shadow-xl'>
-                <Image src="/assets/my-image.png" width="350" height="400" alt="my-image" className='rounded-lg' />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap items-center justify-between px-20 xl:h-[70%] xl:w-[80%] mt-5">
+      <div className="sm:w-[50%]">
+        <h1 className="text-xl">
+          <span className="font-bold">{`HI, I'M EMMANUEL.`}</span>
+          <br />{" "}
+          <span className="">
+            A CREATIVE GRAPHICS DESIGNER
+            <br />I am an enthusiastic and innovative individual with excellent
+            administrative and organizational skills.
+          </span>
+        </h1>
+      </div>
+      <div className="border rounded-lg mr-20 shadow-xl">
+        <Image
+          src="/assets/my-photo.png"
+          width="350"
+          height="400"
+          alt="my-image"
+          className="rounded-lg"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ const NavBar = () => {
 
   return (
     <div className="flex items-center justify-between w-full h-14 xl:h-20 bg-gray-800 text-white px-5 sticky top-0">
-        <div>Emmanuel</div>
+        <div className="ml-5 font-bold">Emmanuel</div>
         <div className="flex items-center justify-between w-[50%] sm:w-[30%] xl:w-[20%]">
             <div className="cursor-pointer hover:text-gray-300 transition duration-300 text-sm sm:text-md xl:font-bold">Gallery</div>
             <div className="bg-white text-gray-900 px-1 py-1 xl:px-2 xl:py-2 rounded-sm text-sm xl:text-md sm:font-bold cursor-pointer hover:bg-gray-500 hover:text-white transition duration-300">{`Let's connect`}</div>
